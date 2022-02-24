b6ee05303f8d9b7888940fa6c7b71c0980599a68
duc.tm <duc.tm@isofhcare.com>
2022-02-24 14:52:39 +0700

    hello123

--- src/components/home/nhanvien/homepage/index.js
+++ src/components/home/nhanvien/homepage/index.js
@@ -1,12 +1,6 @@
 import React from "react";
-// import './homepage.css'
-// import Slider from "./slider/slider"
-// import Banner from "./banner/banner"
-// import Benefit from "./benefit/benefit"
-// import Feature from "./feature/feature"
-// import Promotion from "./promotion/promotion"
-// import Footer from "./footer/footer"
-import { WrapperStyled } from "./styled";
+import './homepage.css'
+// import { WrapperStyled } from "./styled";
 import Slider from "../../homepage/slider/slider";
 import Banner from "../../homepage/banner/banner";
 import Benefit from "../../homepage/benefit/benefit";
@@ -16,13 +10,13 @@ import Footer from "../../homepage/footer/footer";
 
 export default function HomepageChoNhanVien() {
   return (
-    <WrapperStyled>
+    <>
       <Slider />
       <Banner />
       <Benefit />
       <Feature />
       <Promotion />
       <Footer />
-    </WrapperStyled>
+    </>
   );
 }
